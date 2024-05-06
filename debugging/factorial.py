2bcffc36d8289601c7ca4d16969cb7273abe3650
Magnusmajo <alexisoblivion@gmail.com>
2024-05-06 15:14:59 +0000

    Pyton Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
+
 import sys
 
 def factorial(n):
     result = 1
-    while n > 1:
-        result *= n
-        n -= 1
+    for num in range(2, n + 1):
+        result *= num
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: python3 script_name.py <number>")
-    sys.exit(1)
-
-try:
+if __name__ == "__main__":
     num = int(sys.argv[1])
-    if num < 0:
-        print("The number must be non-negative.")
-    else:
-        f = factorial(num)
-        print(f"The factorial of {num} is {f}")
-except ValueError:
-    print("Please provide a valid integer number.")
+    print(f"The factorial of {num} is {factorial(num)}")
 
