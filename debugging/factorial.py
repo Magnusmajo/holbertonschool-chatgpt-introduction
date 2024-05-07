7e3ad6b93fe332eafefd67772a3c076e46a37506
Magnusmajo <alexisoblivion@gmail.com>
2024-05-07 01:30:02 +0000

    Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-
 import sys
 
 def factorial(n):
     result = 1
-    for num in range(2, n + 1):
-        result *= num
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n
     return result
 
-if __name__ == "__main__":
-    num = int(sys.argv[1])
-    print(f"The factorial of {num} is {factorial(num)}")
-
+f = factorial(int(sys.argv[1]))
+print(f)
