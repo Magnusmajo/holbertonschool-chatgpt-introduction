c8a0d5c693953c0130f0c9bf7f7e6a33c58ef6ff
Magnusmajo <alexisoblivion@gmail.com>
2024-09-02 00:13:45 -0300

    0. Debugging - Python Factorial

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -13,4 +13,3 @@ print(f)
 
 """ The factorial function was stuck in an infinite loop. This happens because the value of n is never decremented inside the while loop, so n remains greater than 1 indefinitely.
 o fix this, you need to decrement n within the loop, so the loop will eventually terminate when n becomes 1 """
-
